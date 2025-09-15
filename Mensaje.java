b4427e0caec8a12d6295f1d11d1ddf8192dc97c6
Camilo-Mendoza <camilodelcastillo321@gmail.com>
2025-09-14 21:12:41 -0500

    Practica 01

--- Mensaje.java
+++ Mensaje.java
@@ -1,5 +1,6 @@
 
 import javax.swing.JOptionPane;
+
 public class Mensaje {
     public static void main(String[] args) {
         if (args.length > 0 && args[0].equals("input")) {
---
+++ version3.exs
@@ -0,0 +1,7 @@
+defmodule Mensaje do
+  def main do
+    "Bienvenidos a la empresa Once Ltda"
+    |> IO.puts()
+  end
+end
+Mensaje.main()
